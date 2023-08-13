3f525b83ae179e6c9f655fe9edf903313a967a63
Priyank Shankar <shankarpriyank312002@gmail.com>
2023-08-14 00:49:02 +0530

    Minor changes in the script

--- app/src/main/java/com/priyank/levitate/ui/theme/Color.kt
+++ app/src/main/java/com/priyank/levitate/ui/theme/Color.kt
@@ -6,8 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
-val Purple =  Color(96, 44, 93)
+val Purple = Color(96, 44, 93)
 val Black = Color(0, 0, 0)
 val White = Color(255, 255, 255)
-
-

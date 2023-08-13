933592859bf90287f827012966bb6414175fb8bf
Priyank Shankar <shankarpriyank312002@gmail.com>
2023-08-14 00:47:24 +0530

    Its high time i should start working on this project!

--- app/src/main/java/com/priyank/levitate/MainActivity.kt
+++ app/src/main/java/com/priyank/levitate/MainActivity.kt
@@ -3,15 +3,23 @@ package com.priyank.levitate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.priyank.levitate.ui.theme.FuturaMedium
+import com.priyank.levitate.ui.theme.Lato
 import com.priyank.levitate.ui.theme.LatoLightItalic
 import com.priyank.levitate.ui.theme.LevitateTheme
+import com.priyank.levitate.ui.theme.Purple
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +29,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background,
+                    color = Color.White,
                 ) {
-                    Greeting("Android")
+                    MovingWaveGradient()
                 }
             }
         }
@@ -32,7 +40,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", fontFamily = LatoLightItalic, fontStyle = androidx.compose.ui.text.font.FontStyle.Italic)
+    Text(
+        text = "Hello $name!",
+        fontFamily = LatoLightItalic,
+        fontStyle = androidx.compose.ui.text.font.FontStyle.Italic
+    )
 }
 
 @Preview(showBackground = true)
@@ -41,4 +53,26 @@ fun DefaultPreview() {
     LevitateTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun MovingWaveGradient() {
+    Column {
+        Text(
+            modifier = Modifier.padding(start = 60.dp, top = 100.dp),
+            text = "Welcome to,",
+            fontSize = 24.sp,
+            fontFamily = Lato
+        )
+        Text(
+            modifier = Modifier.padding(start = 60.dp),
+            text = "Levitate",
+            fontSize = 24.sp,
+            fontFamily = FuturaMedium,
+            fontWeight = FontWeight(700),
+            color = Purple
+        )
+    }
+
+
 }

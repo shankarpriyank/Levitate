6d731945f4c600dbc1ff94bd8cc88a0604ddb404
Priyank Shankar <shankarpriyank312002@gmail.com>
2023-08-14 00:47:24 +0530

    Its high time i should start working on this project!

--- app/src/main/java/com/priyank/levitate/ui/theme/Color.kt
+++ app/src/main/java/com/priyank/levitate/ui/theme/Color.kt
@@ -6,9 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val Purple =  Color(96, 44, 93)
 val Black = Color(0, 0, 0)
 val White = Color(255, 255, 255)
-val LightGrey = Color(178, 188, 210)
-val LightBlue = Color(206, 223, 237)
-val LightPurple = Color(167, 172, 217)
-val Purple = Color(102, 109, 172)
+
+
--- app/src/main/java/com/priyank/levitate/ui/theme/Theme.kt
+++ app/src/main/java/com/priyank/levitate/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -17,20 +18,20 @@ private val LightColorPalette = lightColors(
     primaryVariant = Purple700,
     secondary = Teal200,
 
-    /* Other default colors to override
+    // Other default colors to override
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+
 )
 
 @Composable
 fun LevitateTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }

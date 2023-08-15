9e7cf90075f3eef5126339914798c8f7c2058669
Priyank Shankar <shankarpriyank312002@gmail.com>
2023-08-15 23:43:13 +0530

    Made the UI for LoginScreen

---
+++ app/src/main/java/com/priyank/levitate/onboarding/components/GoogleLoginButton.kt
@@ -0,0 +1,104 @@
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.priyank.levitate.R
+import com.priyank.levitate.ui.theme.Shapes
+
+@ExperimentalMaterialApi
+@Composable
+fun SignInButton(
+    modifier: Modifier,
+    text: String,
+    loadingText: String = "Signing in...",
+    icon: Painter,
+    isLoading: Boolean = false,
+    shape: Shape = Shapes.medium,
+    borderColor: Color = Color.LightGray,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    progressIndicatorColor: Color = MaterialTheme.colors.primary,
+    onClick: () -> Unit,
+) {
+    Surface(
+        modifier = modifier.clickable(
+            enabled = !isLoading,
+            onClick = onClick,
+        ),
+        shape = shape,
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = backgroundColor,
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 12.dp,
+                    bottom = 12.dp,
+                )
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 300,
+                        easing = LinearOutSlowInEasing,
+                    ),
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Icon(
+                painter = icon,
+                contentDescription = "SignInButton",
+                tint = Color.Unspecified,
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(text = if (isLoading) loadingText else text)
+            if (isLoading) {
+                Spacer(modifier = Modifier.width(16.dp))
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .height(16.dp)
+                        .width(16.dp),
+                    strokeWidth = 2.dp,
+                    color = progressIndicatorColor,
+                )
+            }
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+@Preview
+fun SignInButtonPreview() {
+    SignInButton(
+        modifier = Modifier,
+        text = "Sign in with Google",
+        loadingText = "Signing in...",
+        isLoading = false,
+        icon = painterResource(id = R.drawable.ic_google_logo),
+        onClick = { },
+    )
+}
--- app/src/main/java/com/priyank/levitate/onboarding/presentation/LoginScreen.kt
+++ app/src/main/java/com/priyank/levitate/onboarding/presentation/LoginScreen.kt
@@ -1,8 +1,71 @@
 package com.priyank.levitate.onboarding.presentation
 
+import SignInButton
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.priyank.levitate.R
+import com.priyank.levitate.ui.theme.FuturaMedium
+import com.priyank.levitate.ui.theme.Lato
+import com.priyank.levitate.ui.theme.Purple
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun LoginScreen(navHostController: NavHostController) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Column {
+                Text(
+                    modifier = Modifier,
+                    text = "It’s time to",
+                    fontSize = 20.sp,
+                    fontFamily = Lato,
+                    fontWeight = FontWeight(400),
+                    textAlign = TextAlign.Start,
+
+                )
+                Text(
+                    text = "Levitate",
+                    fontSize = 48.sp,
+                    fontFamily = FuturaMedium,
+                    fontWeight = FontWeight(700),
+                    color = Purple,
+                )
+            }
+        }
+        SignInButton(
+            modifier = Modifier.align(Alignment.BottomCenter).fillMaxWidth().padding(32.dp),
+            text = "Continue in with Google",
+            loadingText = "Signing in...",
+            isLoading = false,
+            icon = painterResource(id = R.drawable.ic_google_logo),
+            onClick = { },
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLoginScreen() {
+    LoginScreen(navHostController = NavHostController(LocalContext.current))
 }
